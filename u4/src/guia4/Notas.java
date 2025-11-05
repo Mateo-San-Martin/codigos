240217ba0e7e0a10464f179d9986f7b8586042ef
mateo <matteosmm17@gmail.com>
2025-11-05 17:00:43 -0300

    correccion

--- u4/src/guia4/Notas.java
+++ u4/src/guia4/Notas.java
@@ -3,14 +3,14 @@ package guia4;
 public class Notas {
     double [][] calificaciones;
 
-    public double calcularPromedioPorEstudiante(double [][] calificaciones, int numero){
+    public double calcularPromedioPorEstudiante(int numero){
         double promedio = 0;
         for (int i = 0; i < calificaciones[numero].length; i++) {
             promedio += calificaciones[numero][i];
         }
         return promedio/calificaciones[numero].length;
     }
-    public double calcularPromedioPorMateria(double [][] calificaciones, int numero){
+    public double calcularPromedioPorMateria(int numero){
         double promedio = 0;
         for (int i = 0; i < calificaciones.length; i++) {
             promedio += calificaciones[i][numero];
